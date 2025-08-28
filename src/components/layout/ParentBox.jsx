340737d1cff55a951a5f634083984f8682098425
NapiTech-dev <felicianoloran@gmail.com>
2025-08-28 21:07:24 +0000

    still styling

--- src/components/layout/ParentBox.jsx
+++ src/components/layout/ParentBox.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Dashboard from "../Dashboard.jsx";
+import SideBar from "../SideBar.jsx";
 import "../../styles/ParentBox.css";
 
 const ParentBox = () => {
   return (
     <div className="box-style">
+      <SideBar />
       <Dashboard />
     </div>
   );
