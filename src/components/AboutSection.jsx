8efc138287e32aba7b49ce70b8d1f6fd791f408d
NapiTech-dev <felicianoloran@gmail.com>
2025-08-29 22:54:32 +0000

    done with about section

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -5,6 +5,16 @@ const AboutSection = () => {
   return (
     <div className="about-styles">
       <h2 className="about-title">About Me</h2>
+      <p className="about-text">
+        I am Full stack web developer and UI/UX Designer from Equatorial Guinea
+        based in Ghana, I specialise in creating dynamic, responsive websites
+        and applications that provide seamless user experiences. I enjoy turning
+        complex problems into simple, beautiful and intuitive solutions. My job
+        is to build your website so that it is functional and user-friendly but
+        at the same time attractive. Moreover, I add personal touch to your
+        product and make sure that is eye-catching and easy to use. My aim is to
+        bring across your message and identity in the most creative way.
+      </p>
     </div>
   );
 };
