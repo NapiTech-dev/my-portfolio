4a58a795dcc6c4c56d87e0bcefe720c64a162ec5
NapiTech-dev <felicianoloran@gmail.com>
2025-08-30 15:31:50 +0000

    still styling skill box

--- src/components/SkillSection.jsx
+++ src/components/SkillSection.jsx
@@ -1,12 +1,24 @@
-import React from 'react'
+import React from "react";
 import "../styles/SkillSection.css";
 
 const SkillSection = () => {
   return (
-    <div className="skills-box">
-        <h2 className="skills-title">My Skills</h2>
+    <div className="skills-section">
+      <h2 className="skills-title">My Skills</h2>
+      <div className="skills-box">
+        <div className="skill-box">
+          <div className="icon-background"></div>
+          <div className="skill-details">
+            <div className="skill-title">Web App</div>
+            <div className="skill-text">
+              I build scalable, efficient, and responsive web applications using
+              modern frameworks and best practices.
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default SkillSection;
