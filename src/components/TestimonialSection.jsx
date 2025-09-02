83e52f998afa3ecf28a0dcd159a649e0b7155fde
NapiTech-dev <felicianoloran@gmail.com>
2025-09-02 19:17:00 +0000

    still styling testimonials box

--- src/components/TestimonialSection.jsx
+++ src/components/TestimonialSection.jsx
@@ -3,8 +3,14 @@ import "../styles/TestimonialSection.css";
 
 const TestimonialSection = () => {
   return (
-    <div className="Testimonial-section">
-      <h2>Testimonial</h2>
+    <div className="testimonial-section">
+      <h2 className="testmony-title">Testimonials</h2>
+      <div className="testimonies-box">
+        <div className="testimony-box">
+            <div className="tesimony-image-box"></div>
+            <div className=""></div>testimony-text-box
+        </div>
+      </div>
     </div>
   );
 };
